ca9831787aa82fb47eb538f08636b187bf37fa30
veedanny <vinittasyvia3@gmail.com>
2024-07-11 20:40:01 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  const saturdayFunResult = saturdayFun();
+console.log(saturdayFunResult); // Output: "This Saturday, I want to roller-skate!"
+
+const mondayWorkResult = mondayWork();
+console.log(mondayWorkResult); // Output: "This Monday, I will go to the office."
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer")); // Output: "You are !!!a dedicated programmer!!!"
+
+const anotherEncouragingPromptFunction = wrapAdjective("%");
+console.log(anotherEncouragingPromptFunction("a skilled artist")); // Output: "You are %a skilled artist%!"
+
